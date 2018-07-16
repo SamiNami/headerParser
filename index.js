876cc9b1bbd01019f324d28d4a0f1718a6161f81
sami peralahti <sami.peralahti@vainu.io>
2018-07-16 12:07:33 -0700

    testing

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+// if not timestamp is provided use the time of right now
+app.get('/api/timestamp/', (req, res) => {
+    const date = new Date();
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+app.get('/api/timestamp/:date_string', (req, res) => {
+    const { date_string } = req.params;
+    let date = Number(date_string * 1000) || date_string;
+    date = new Date(date);
+
+    if (isNaN(date)) {
+        res.json({ error: 'Invalid Date' });
+        return;
+    }
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
