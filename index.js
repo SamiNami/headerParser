9421777d9a1c2ccef7a40740a895f5392dbc2131
sami peralahti <sami.peralahti@vainu.io>
2018-07-16 12:29:47 -0700

    some changes

--- index.js
+++ index.js
@@ -3,22 +3,16 @@ const express = require('express');
 const app = express();
 
 // if not timestamp is provided use the time of right now
-app.get('/api/timestamp/', (req, res) => {
+app.get('/api/whoami', (req, res) => {
     const date = new Date();
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    console.log(req.headers);
+    res.json({
+        ipaddress: req.connection.remoteAddress,
+        language: 'en-US,en;q=0.5',
+        software:
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
+    });
 });
 
-app.get('/api/timestamp/:date_string', (req, res) => {
-    const { date_string } = req.params;
-    let date = Number(date_string * 1000) || date_string;
-    date = new Date(date);
-
-    if (isNaN(date)) {
-        res.json({ error: 'Invalid Date' });
-        return;
-    }
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT);
