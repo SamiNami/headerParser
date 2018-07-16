f26468b70c9a127dbbe9650ab514a62e78d78e40
sami peralahti <sami.peralahti@vainu.io>
2018-07-16 14:32:44 -0700

    testing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
-const requestIp = require('request-ip');
 
 app.get('/api/whoami', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip);
     res.json({
-        ipaddress: req.ip,
+        ipaddress: ip,
         language: 'en-US,en;q=0.5',
         software:
             'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
